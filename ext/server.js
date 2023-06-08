88a7ab143355aec9c252eea72f5d0ad6fc67a31c
Whitewolf24 <geo_marinos@yahoo.com>
2023-06-08 21:24:25 +0300

    0.1

---
+++ ext/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+//const path = require('path');
+
+const express = require('express');
+const expressui = require('express-ejs-layouts');
+const serv = express();
+const port = 2700 || process.env.port;
+
+serv.use(express.static('public'));
+serv.use(expressui);
+serv.set('layout', '../layouts/main');
+serv.set('view engine', 'ejs');
+//serv.use(express.static(path.join(__dirname + './public')));
+
+serv.use('/', require('./routes/main.js'));
+
+serv.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
